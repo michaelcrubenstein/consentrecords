4fdacbc37d4440cf6f60027e78ecfad44eb6bf4e
michaelcrubenstein <michaelcrubenstein@gmail.com>
2015-12-20 22:46:49 -0500

    Fix bugs that prevented display of transactions in admin

--- consentrecords/admin.py
+++ consentrecords/admin.py
@@ -5,26 +5,50 @@ from django import forms
 
 from consentrecords.models import Instance, DeletedInstance, Value, DeletedValue, Transaction
 
-class InstanceInline(admin.StackedInline):
+class InstanceInline(admin.TabularInline):
     model = Instance
     extra = 0
+    fieldsets = (
+        (None, {'fields': ('id', 'typeID', 'parent', '_description', 'transaction')}),
+    )
+    readonly_fields = ('id', 'typeID', 'parent', '_description', 'transaction')
+    show_change_link = True
     
-class DeletedInstanceInline(admin.StackedInline):
+class DeletedInstanceInline(admin.TabularInline):
     model = DeletedInstance
     extra = 0
+    fieldsets = (
+        (None, {'fields': ('id', 'transaction')}),
+    )
+    readonly_fields = ('id', 'transaction',)
+    show_change_link = True
     
-class ValueInline(admin.StackedInline):
+class ValueInline(admin.TabularInline):
     model = Value
     extra = 0
+    list_display = ('id', 'instance', 'fieldID', 'stringValue', 'referenceValue', 'position', 'transaction')
+    fieldsets = (
+        (None, {'fields': ('id', 'instance', 'fieldID', 'stringValue', 'referenceValue', 'position', 'transaction')}),
+    )
+    readonly_fields = ('id','instance', 'fieldID', 'stringValue', 'referenceValue', 'position','transaction')
+    show_change_link = True
     
-class InstanceValueInline(admin.StackedInline):
+class InstanceValueInline(admin.TabularInline):
     model = Value
     fk_name = 'instance'
     extra = 0
+    list_display = ('id', 'instance', 'fieldID', 'stringValue', 'referenceValue', 'position', 'transaction')
+    fieldsets = (
+        (None, {'fields': ('id', 'instance', 'fieldID', 'stringValue', 'referenceValue', 'position', 'transaction')}),
+    )
+    readonly_fields = ('id','instance', 'fieldID', 'stringValue', 'referenceValue', 'position','transaction')
+    show_change_link = True
     
-class DeletedValueInline(admin.StackedInline):
+class DeletedValueInline(admin.TabularInline):
     model = DeletedValue
     extra = 0
+    readonly_fields = ('id', 'transaction',)
+    show_change_link = True
     
 class DeletedInstanceAdmin(admin.ModelAdmin):
 
@@ -70,7 +94,7 @@ class TransactionAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('id', 'user', 'creation_time', 'time_zone_offset')}),
     )
-    readonly_fields = ('id','user', 'creation_time', 'time_zone_offset')
+    readonly_fields = ('id', 'user', 'creation_time', 'time_zone_offset')
     search_fields = ('id',)
     
     inlines = [InstanceInline, DeletedInstanceInline, ValueInline, DeletedValueInline]
