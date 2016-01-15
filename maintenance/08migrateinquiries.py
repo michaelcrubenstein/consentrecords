2d462f1caf20b6fac60fd995f1ac1562061f6766
michaelcrubenstein <michaelcrubenstein@gmail.com>
2016-01-15 13:41:31 -0500

    Fix bug in previous commit to handle null instance.

--- maintenance/08migrateinquiries.py
+++ maintenance/08migrateinquiries.py
@@ -16,10 +16,7 @@ if __name__ == "__main__":
     django.setup()
 
     timezoneoffset = -int(tzlocal.get_localzone().utcoffset(datetime.datetime.now()).total_seconds()/60)
-    if len(sys.argv) > 1:
-        username = sys.argv[1]
-    else:
-        username = input('Email Address: ')
+    username = sys.argv[1] if len(sys.argv) > 1 else input('Email Address: ')
     password = getpass.getpass("Password: ")
 
     user = authenticate(username=username, password=password)
@@ -27,11 +24,11 @@ if __name__ == "__main__":
     with transaction.atomic():
         transactionState = TransactionState(user, timezoneoffset)
         Terms.initialize(transactionState)
-        
+
         f = Instance.objects.filter(typeID=Terms.getNamedInstance('Inquiry'), deleteTransaction__isnull=True,
                                     value__fieldID=Terms.email,
                                     value__deleteTransaction__isnull=True)
-        
+
         print ("Inquiry Count: %s" % f.count())
         userInfo = UserInfo(user)
         for inquiry in f:
@@ -43,9 +40,12 @@ if __name__ == "__main__":
             l = pathparser.selectAllObjects(path, userInfo=userInfo, securityFilter=userInfo.findFilter)
             if len(l):
                 inquiries = inquiry.parent
-                print("l: %s" % str(l))
-                transactionState = TransactionState(l[0].user, timezoneoffset)
-                inquiries.addReferenceValue(Terms.user, l[0], 0, transactionState)
-                print("update inquiry for session %s: user %s" % (inquiry.parent.parent, l[0]))
-            inquiry.markAsDeleted(transactionState)
+                if inquiries:
+                    print("l: %s" % str(l))
+                    transactionState = TransactionState(l[0].user, timezoneoffset)
+                    inquiries.addReferenceValue(Terms.user, l[0], 0, transactionState)
+                    print("update inquiry for session %s: user %s" % (inquiry.parent.parent, l[0]))
+                    inquiry.markAsDeleted(transactionState)
+                else:
+                    print("no parent for %s" % str(inquiry))
                      
