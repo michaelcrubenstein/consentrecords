9e8ae808e4b3db18527346df6cfc10436f94533e
michaelcrubenstein <michaelcrubenstein@gmail.com>
2016-05-09 15:22:33 -0400

    Add domains and stages to service hierarchy

---
+++ data/07updateservices.py
@@ -0,0 +1,38 @@
+import datetime
+import django
+import tzlocal
+import getpass
+import traceback
+import sys
+import csv
+import re
+
+from django.db import transaction
+from django.contrib.auth import authenticate
+
+from consentrecords.models import *
+from consentrecords import pathparser
+from consentrecords import instancecreator
+
+# Delete the Education Service
+ed = Instance.objects.get(typeID=terms['Service'],description__text='Education')
+vs = Value.objects.filter(referenceValue=ed, deleteTransaction__isnull=True)
+
+# Rename "Play Baseball" to "Baseball Playing"
+# Rename "Play Basketball" to "Basketball Playing"
+# Rename "Play Baseball" to "Baseball Playing"
+# Rename "Play Chess" to "Chess Playing"
+# Rename "Play Football" to "Football Playing"
+# Rename "Play Soccer" to "Soccer Playing"
+# Rename "Play Squash" to "Squash Playing"
+# Rename "Play Tennis" to "Tennis Playing"
+# Rename "Play Volleyball" to "Volleyball Playing"
+# Rename "Karate" to "Karate Training"
+# Rename "Animals" to "Animal Shelter Volunteer"
+# Rename "Art Lessons" to "Drawing Lessing"
+# Rename "Coach Baseball" to "Baseball Coaching"
+# Rename "Coach Football" to "Football Coaching"
+# Rename "Coach Soccer" to "Soccer Coaching"
+# Rename "Coach Tennis" to "Tennis Coaching"
+
+# Incorporate the Soccer Service into Play Soccer
