593fa04775a8b73e04abf1bbbd3ba7c6bb9bb512
michaelcrubenstein <michaelcrubenstein@gmail.com>
2016-05-10 11:57:50 -0400

    Remove renaming operations after completing them on the public website

--- data/07updateservices.py
+++ data/07updateservices.py
@@ -18,29 +18,6 @@ from consentrecords import instancecreator
 ed = Instance.objects.get(typeID=terms['Service'],description__text='Education')
 vs = Value.objects.filter(referenceValue=ed, deleteTransaction__isnull=True)
 
-# Rename "Play Baseball" to "Baseball Playing"
-# Rename "Play Basketball" to "Basketball Playing"
-# Rename "Play Baseball" to "Baseball Playing"
-# Rename "Play Chess" to "Chess Playing"
-# Rename "Play Football" to "Football Playing"
-# Rename "Play Soccer" to "Soccer Playing"
-# Rename "Play Squash" to "Squash Playing"
-# Rename "Play Tennis" to "Tennis Playing"
-# Rename "Play Volleyball" to "Volleyball Playing"
-# Rename "Karate" to "Karate Training"
-# Rename "Animals" to "Animal Shelter Volunteer"
-# Rename "Art Lessons" to "Drawing Lessing"
-# Rename "Coach Baseball" to "Baseball Coaching"
-# Rename "Coach Football" to "Football Coaching"
-# Rename "Coach Soccer" to "Soccer Coaching"
-# Rename "Coach Tennis" to "Tennis Coaching"
-# Rename "Political Organizing" to "Political Organizing Volunteer"
-# Rename "Teach English" to "English Teaching"
-# Rename "Teach Math" to "Math Teaching"
-# Rename "Teach Spanish" to "Spanish Teaching"
-# Rename "Learn English" to "English Class"
-# Rename "Learn Spanish" to "Spanish Class"
-
 # Incorporate the Basketball Service into Play Basketball
 # Incorporate the Football Service into Play Football
 # Incorporate the Soccer Service into Play Soccer
