486caa74f0adbce56292fe2b0967bcf89cbae75b
michaelcrubenstein <michaelcrubenstein@gmail.com>
2016-09-11 17:43:18 -0400

    Eliminate extra print statement in server code

--- consentrecords/views.py
+++ consentrecords/views.py
@@ -36,7 +36,6 @@ def handler404(request):
 
 
 def handler500(request):
-    print('handler500')
     response = render_to_response('500.html', {},
                                   context_instance=RequestContext(request))
     response.status_code = 500
