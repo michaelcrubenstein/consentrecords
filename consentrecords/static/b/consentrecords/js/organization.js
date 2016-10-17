ece4be0a20acf996144cfdbee9b481283eabdce6
michaelcrubenstein <michaelcrubenstein@gmail.com>
2016-10-16 20:18:35 -0400

    Check for case of specifying path without parent

--- consentrecords/static/b/consentrecords/js/organization.js
+++ consentrecords/static/b/consentrecords/js/organization.js
@@ -162,9 +162,9 @@ function getUserName(user)
 
 function getPathDescription(path)
 {
-	return (path.cell.parent && getUserName(path.cell.parent)) ||
+	return (path.cell && path.cell.parent && getUserName(path.cell.parent)) ||
 			path.getDatum("_name") ||
-		   (path.cell.parent && path.cell.parent.getDescription()) ||
+		   (path.cell && path.cell.parent && path.cell.parent.getDescription()) ||
 		   null;
 }
 
