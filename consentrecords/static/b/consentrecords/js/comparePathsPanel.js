1b0272b36fd9414de41747fc00688975e3f4805a
michaelcrubenstein <michaelcrubenstein@gmail.com>
2016-12-11 15:18:21 -0500

    Fix bug to correct width of underlying rectangles for comparePaths experiences.

--- consentrecords/static/b/consentrecords/js/comparePathsPanel.js
+++ consentrecords/static/b/consentrecords/js/comparePathsPanel.js
@@ -283,7 +283,8 @@ var ComparePath = (function() {
 			.attr('height', "{0}em".format(this.flagHeightEM))
 			.attr('width', function(fd)
 				{
-					return $(this.parentNode).children('text').outerWidth() + 5;
+					return $(this.parentNode).children('text').outerWidth() + 
+						(2 * _this.textDetailLeftMargin);
 				});	
 		
 		/* Restore the sort order to startDate/endDate */
