3e6b95332c55c6d4c895990c02c8e7c04f165c4f
michaelcrubenstein <michaelcrubenstein@gmail.com>
2017-06-23 12:23:21 -0400

    Add exception handling to update operation in script

--- Testing/python/07createUpdateDeleteService.py
+++ Testing/python/07createUpdateDeleteService.py
@@ -1,6 +1,7 @@
 import django; django.setup()
 from django.db import transaction
-import profile
+import traceback
+import logging
 from uuid import UUID
 from consentrecords.models import *
 from parse.cssparser import parser as cssparser
@@ -28,25 +29,29 @@ with transaction.atomic():
     newItem = Service.create(data, context, newIDs=newIDs)
     print(str(newItem), newIDs)
 
-with transaction.atomic():
-    context = Context('en', mr)
-    data = {'stage': 'Volunteering',
-            'names': [{'id': newIDs['1'], 'text': 'Bar Service'},
-                      {'clientID': '8.2', 'text': 'Biff Service', 'languageCode': 'sp'}],
-            'organization labels': [{'id': newIDs['2'], 'delete': 'delete'}, 
-                                    {'clientID': '1', 'text': 'Bar Organization Label', 'languageCode': 'en'}],
-            'site labels': [{'id': newIDs['3'], 'text': 'Foo 2 Site Label'}, 
-                            {'clientID': '3', 'text': 'Biff Site Label', 'languageCode': 'en'}],
-            'offering labels': [{'id': newIDs['5'], 'text': 'Foo 2 offering label'}, 
-                                {'clientID': '6', 'text': 'Biff Organization Label', 'languageCode': 'sp'}],
-            'services': [{'id': newIDs['7'], 'service': 'service[name>text=Volunteer]'},
-                         {'clientID': '9', 'service': 'service[name>text=Skills]'}],
-           }
-    newIDs2 = {}
-    context = Context('en', mr)
-    i = newItem
-    i.update(data, context, newIDs2)
-    print(i)
+try:
+	with transaction.atomic():
+		context = Context('en', mr)
+		data = {'stage': 'Volunteering',
+				'names': [{'id': newIDs['1'], 'text': 'Bar Service'},
+						  {'clientID': '8.2', 'text': 'Biff Service', 'languageCode': 'sp'}],
+				'organization labels': [{'id': newIDs['2'], 'delete': 'delete'}, 
+										{'clientID': '1', 'text': 'Bar Organization Label', 'languageCode': 'en'}],
+				'site labels': [{'id': newIDs['3'], 'text': 'Foo 2 Site Label'}, 
+								{'clientID': '3', 'text': 'Biff Site Label', 'languageCode': 'en'}],
+				'offering labels': [{'id': newIDs['5'], 'text': 'Foo 2 offering label'}, 
+									{'clientID': '6', 'text': 'Biff Organization Label', 'languageCode': 'sp'}],
+				'services': [{'id': newIDs['7'], 'service': 'service[name>text=Volunteer]'},
+							 {'clientID': '9', 'service': 'service[name>text=Skills]'}],
+			   }
+		newIDs2 = {}
+		context = Context('en', mr)
+		i = newItem
+		i.update(data, context, newIDs2)
+		print(i)
+except Exception as e:
+	logger = logging.getLogger(__name__)
+	logger.error("%s" % traceback.format_exc())
         
 with transaction.atomic():
     context = Context('en', mr)
