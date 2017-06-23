cca8d7acc78bbdec8e8730240fbe603134489b3f
michaelcrubenstein <michaelcrubenstein@gmail.com>
2017-06-23 12:27:05 -0400

    Combine create update and delete tests for CommentPrompt

--- Testing/python/09createCommentPrompt.py
+++ Testing/python/09createCommentPrompt.py
@@ -1,6 +1,7 @@
 import django; django.setup()
 from django.db import transaction
-import profile
+import traceback
+import logging
 from uuid import UUID
 from consentrecords.models import *
 from parse.cssparser import parser as cssparser
@@ -22,3 +23,23 @@ with transaction.atomic():
     context = Context('en', mr)
     newItem = CommentPrompt.create(data, context, newIDs=newIDs)
     print(str(newItem), newIDs)
+
+try:
+	with transaction.atomic():
+		context = Context('en', mr)
+		data = {'translations': [{'id': newIDs['1'], 'text': 'Bar Comment Prompt'},
+						         {'id': newIDs['2'], 'text': 'Biff Comment Prompt', 'languageCode': 'zh'}],
+			   }
+		newIDs2 = {}
+		context = Context('en', mr)
+		i = newItem
+		i.update(data, context, newIDs2)
+		print(i)
+except Exception as e:
+	logger = logging.getLogger(__name__)
+	logger.error("%s" % traceback.format_exc())
+        
+with transaction.atomic():
+    context = Context('en', mr)
+    newItem.markDeleted(context)
+    print("Comment Prompt deleted")
