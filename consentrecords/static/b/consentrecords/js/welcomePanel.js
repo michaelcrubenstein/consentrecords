abe23fc9ea8b44d55f2fc325664ecd5f02ea8c92
michaelcrubenstein <michaelcrubenstein@gmail.com>
2017-10-31 07:30:59 -0400

    Fix bug positioning main panels on landing page

--- consentrecords/static/b/consentrecords/js/welcomePanel.js
+++ consentrecords/static/b/consentrecords/js/welcomePanel.js
@@ -110,7 +110,7 @@ var WelcomePanel = (function () {
 	
 	WelcomePanel.prototype.promptMargin = function()
 	{
-		return 2 * parseInt($(this.mainDiv.node()).css('font-size'));
+		return 1.5 * parseInt($(this.mainDiv.node()).css('font-size'));
 	}
 	
 	WelcomePanel.prototype.promptBottomMargin = function()
@@ -274,7 +274,7 @@ var WelcomePanel = (function () {
 		var _this = this;
 		var $mainDiv = $(this.mainDiv.node());
 		
-		var margin = 1.5 * parseFloat($mainDiv.css('font-size'));
+		var margin = this.promptMargin();
 		this.onResizePrompts();
 
 		var firstChild = $(this.promptScrollArea.node()).children('panel').get(0);
